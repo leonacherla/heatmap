e22a30947a93c2eda8ad327a930c193e06243cad
leonacherla <leonacherla@gmail.com>
2015-11-19 23:40:25 -0800

    Commenting Twitter Streaming API

--- app.js
+++ app.js
@@ -67,76 +67,76 @@ app.use(function(err, req, res, next) {
 });
 
 /* SOCKET.IO*/
-io.on('connection', function(socket){
-  console.log('a user connected');
-    var clientIp = socket.request.connection.remoteAddress
-    console.log(clientIp)
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//     var clientIp = socket.request.connection.remoteAddress
+//     console.log(clientIp)
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
 
 
 /* TWITTER*/
 
-t.on('tweet', function (tweet) {
-	if (tweet['text'].toLowerCase().indexOf("follow") == -1) {
-		if (tweet['coordinates'] || tweet['place']) {
-			console.log('---------- BEGIN NEW TWEET ----------------------');
-			//console.log (tweet);
-			if (tweet['quoted_status']) {
-				console.log ("REPLY FOUND:");
-				console.log (tweet['quoted_status']);
-				console.log("REPLY NAME: " + tweet['quoted_status']['user']['name']);
-				console.log("REPLY SCREENNAME: @" + tweet['quoted_status']['user']['screen_name']);
-				console.log("REPLY TEXT: " + tweet['quoted_status']['text']);
-				console.log("REPLY PIC: " + tweet['quoted_status']['user']['profile_image_url_https']);
-			}
-			console.log('Name: ' + tweet['user']['name']);
-			console.log('ScreenName: @' + tweet['user']['screen_name']);
-			var picUrl, picLink;
-			if (tweet['entities']['media']) {
-				picUrl = tweet['entities'].media[0].media_url_https;
-				picLink = tweet['entities'].media[0].expanded_url;
-			}
-
-			if (tweet['coordinates']) {
-				console.log('COORDINATE TYPE:  COORDINATES');
-				console.log('Coordinates (coordinates): ' + tweet['coordinates']['coordinates']); //OK
-				var latitude = tweet['coordinates']['coordinates'][1];
-				var longitude = tweet['coordinates']['coordinates'][0];
-			} else if (tweet['place']) {
-				if(tweet['place']['bounding_box']) {
-					if(tweet['place']['bounding_box']['type'] === 'Polygon') {
-						console.log('COORDINATE TYPE:  PLACE');
-						console.log('Coordinates (Place): ' + tweet['place']['bounding_box'].coordinates[0]);
-						var coordinates = tools.center(tweet['place']['bounding_box'].coordinates[0]);
-						var latitude = coordinates[1].toFixed(6);
-						var longitude = coordinates[0].toFixed(6);
-						console.log('FINAL Latitude: ' + latitude);
-						console.log('FINAL Longitude: ' + longitude);
-					}
-				}
-			}
-			console.log('---------- END NEW TWEET ----------------------');
-			io.emit('tweet', {
-				name : tweet['user']['name'],
-				screenName: tweet['user']['screen_name'],
-				body: tools.linkify(tweet['text']),
-				latitude: latitude,
-				longitude: longitude,
-				avatar: tweet['user']['profile_image_url_https'],
-				picUrl: picUrl,
-				picLink: picLink
-			});
-		}
-	}
-});
-
-t.on('error', function (err) {
-  console.log('FATAL ERROR: Oh no :(')
-});
-
-t.track('paris');
+// t.on('tweet', function (tweet) {
+// 	if (tweet['text'].toLowerCase().indexOf("follow") == -1) {
+// 		if (tweet['coordinates'] || tweet['place']) {
+// 			console.log('---------- BEGIN NEW TWEET ----------------------');
+// 			//console.log (tweet);
+// 			if (tweet['quoted_status']) {
+// 				console.log ("REPLY FOUND:");
+// 				console.log (tweet['quoted_status']);
+// 				console.log("REPLY NAME: " + tweet['quoted_status']['user']['name']);
+// 				console.log("REPLY SCREENNAME: @" + tweet['quoted_status']['user']['screen_name']);
+// 				console.log("REPLY TEXT: " + tweet['quoted_status']['text']);
+// 				console.log("REPLY PIC: " + tweet['quoted_status']['user']['profile_image_url_https']);
+// 			}
+// 			console.log('Name: ' + tweet['user']['name']);
+// 			console.log('ScreenName: @' + tweet['user']['screen_name']);
+// 			var picUrl, picLink;
+// 			if (tweet['entities']['media']) {
+// 				picUrl = tweet['entities'].media[0].media_url_https;
+// 				picLink = tweet['entities'].media[0].expanded_url;
+// 			}
+// 
+// 			if (tweet['coordinates']) {
+// 				console.log('COORDINATE TYPE:  COORDINATES');
+// 				console.log('Coordinates (coordinates): ' + tweet['coordinates']['coordinates']); //OK
+// 				var latitude = tweet['coordinates']['coordinates'][1];
+// 				var longitude = tweet['coordinates']['coordinates'][0];
+// 			} else if (tweet['place']) {
+// 				if(tweet['place']['bounding_box']) {
+// 					if(tweet['place']['bounding_box']['type'] === 'Polygon') {
+// 						console.log('COORDINATE TYPE:  PLACE');
+// 						console.log('Coordinates (Place): ' + tweet['place']['bounding_box'].coordinates[0]);
+// 						var coordinates = tools.center(tweet['place']['bounding_box'].coordinates[0]);
+// 						var latitude = coordinates[1].toFixed(6);
+// 						var longitude = coordinates[0].toFixed(6);
+// 						console.log('FINAL Latitude: ' + latitude);
+// 						console.log('FINAL Longitude: ' + longitude);
+// 					}
+// 				}
+// 			}
+// 			console.log('---------- END NEW TWEET ----------------------');
+// 			io.emit('tweet', {
+// 				name : tweet['user']['name'],
+// 				screenName: tweet['user']['screen_name'],
+// 				body: tools.linkify(tweet['text']),
+// 				latitude: latitude,
+// 				longitude: longitude,
+// 				avatar: tweet['user']['profile_image_url_https'],
+// 				picUrl: picUrl,
+// 				picLink: picLink
+// 			});
+// 		}
+// 	}
+// });
+// 
+// t.on('error', function (err) {
+//   console.log('FATAL ERROR: Oh no :(')
+// });
+// 
+// t.track('paris');
 
 module.exports = app;
